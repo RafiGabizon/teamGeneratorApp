0623ee07f32585a449d3135e1c6d5e33438f5de3
RafiGabizon <rafigabizon11@gmail.com>
2024-08-11 11:35:22 +0300

    last fixes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const Player = require('./models/Player');
 
 const app = express();
@@ -26,6 +26,15 @@ app.get('/api/players', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+// Serve static files from the React app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
