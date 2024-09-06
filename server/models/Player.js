f655f2f577bf16c8ef9b58815fb0c76420a12a4e
RafiGabizon <rafigabizon11@gmail.com>
2024-09-06 11:22:21 +0300

    fixes and deleting server side

--- server/models/Player.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const playerSchema = new mongoose.Schema({
-  name: String,
-  playStyle: String,
-  level: Number
-});
-
-module.exports = mongoose.model('Player', playerSchema);
